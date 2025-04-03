236b83049581f36dde8918a7c06795359fbe8c13
Arnau Miragall <arnaum03@gmail.com>
2025-04-03 17:34:23 +0200

    [SIMULATION] Scaled planets
    + some environmental upgrades

--- Assets/Scripts/SimulationCore/SimulationConstants.cs
+++ Assets/Scripts/SimulationCore/SimulationConstants.cs
@@ -1,5 +1,5 @@
 public static class SimulationConstants
 {
     public static readonly float GravitationalConstant = 39.478f;
-    public static float ScaleFactor = 3f;
+    public static float ScaleFactor = 32f;
 }
